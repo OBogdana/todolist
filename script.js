5c780d29cadfc3b95f391817a7233908791ef7b6
Bohdana <bohdana@MacBook-Pro-Apple.local>
2024-02-22 21:21:39 +0200

    creation styles.css

--- script.js
+++ script.js
@@ -4,18 +4,10 @@ const taskList = document.getElementById('taskList');
 
 addTaskBtn.addEventListener('click', () =>{
     const taskText = taskInput.value.trim();
-    if (taskText !== '') {
         const taskItem = document.createElement('li');
         taskItem.textContent = taskText;
         taskList.appendChild(taskItem);
         taskInput.value = '';
-    } else {
-        alert('Введіть завдання');
-    }
+
 });
 
-taskList.addEventListener('click', (event) => {
-    if (event.target.tagName === 'LI') {
-        event.target.remove();
-    }
-});
